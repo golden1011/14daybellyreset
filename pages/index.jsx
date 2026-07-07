7b13b1481192c5e5613cc7e1e3b73dab4f2668a9
Anthony La Rocca <golden10@gmail.com>
2026-07-06 21:54:09 -0400

    Add Meta Pixel events

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,8 @@
+import Script from "next/script";
 import SalesEnhancements from "./SalesEnhancements";
 import { getSalesPageHtml } from "../lib/dcPage.js";
+
+const pixelId = "1343158414610855";
 
 export async function getStaticProps() {
   return {
@@ -12,8 +15,39 @@ export async function getStaticProps() {
 export default function SalesPage({ html }) {
   return (
     <>
+      <MetaPixel event="PageView" />
       <main dangerouslySetInnerHTML={{ __html: html }} />
       <SalesEnhancements />
+    </>
+  );
+}
+
+function MetaPixel({ event }) {
+  return (
+    <>
+      <Script id="meta-pixel-base" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${pixelId}');
+          fbq('track', '${event}');
+        `}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=${pixelId}&ev=${event}&noscript=1`}
+          alt=""
+        />
+      </noscript>
     </>
   );
 }
--- pages/thank-you.jsx
+++ pages/thank-you.jsx
@@ -1,4 +1,7 @@
+import Script from "next/script";
 import { getThankYouPageHtml } from "../lib/dcPage.js";
+
+const pixelId = "1343158414610855";
 
 export async function getStaticProps() {
   return {
@@ -9,5 +12,40 @@ export async function getStaticProps() {
 }
 
 export default function ThankYouPage({ html }) {
-  return <main dangerouslySetInnerHTML={{ __html: html }} />;
+  return (
+    <>
+      <MetaPixelPurchase />
+      <main dangerouslySetInnerHTML={{ __html: html }} />
+    </>
+  );
+}
+
+function MetaPixelPurchase() {
+  return (
+    <>
+      <Script id="meta-pixel-purchase" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${pixelId}');
+          fbq('track', 'Purchase', {currency: 'USD', value: 27.00});
+        `}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=${pixelId}&ev=Purchase&cd[currency]=USD&cd[value]=27.00&noscript=1`}
+          alt=""
+        />
+      </noscript>
+    </>
+  );
 }
