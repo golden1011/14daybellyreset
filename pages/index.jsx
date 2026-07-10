0e49744b3d7527bfdf2d6c09fecba5a1fe0ba37d
Anthony La Rocca <golden10@gmail.com>
2026-07-10 18:19:58 -0400

    Refine hero layout and page title

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import MetaEvent from "../components/MetaEvent";
 import SalesEnhancements from "./SalesEnhancements";
 import { getSalesPageHtml } from "../lib/dcPage.js";
@@ -13,6 +14,9 @@ export async function getStaticProps() {
 export default function SalesPage({ html }) {
   return (
     <>
+      <Head>
+        <title>The 14 Day Belly Reset</title>
+      </Head>
       <MetaEvent eventName="PageView" />
       <main dangerouslySetInnerHTML={{ __html: html }} />
       <SalesEnhancements />
